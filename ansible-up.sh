7e7b33f692e555676da616bba7461bc1ce24ef8f
svintus <ilya.semenuaka@gmail.com>
2025-04-14 00:30:11 +0300

    Добавление новых ansible ролей и добавление service-server

--- ansible-up.sh
+++ ansible-up.sh
@@ -1,19 +1,21 @@
-#!/bin/bash
-set -e
+# Тут все надо менять!
 
-cd ansible
+# #!/bin/bash
+# set -e
 
-ansible-playbook -i inventory/hosts.ini all.yml --tags preconfig
-ansible-playbook -i inventory/hosts.ini all.yml --tags users-init --limit server-staging
+# cd ansible
 
-read -p "Введите токен для server-staging: " runner_token
-ansible-playbook -i inventory/hosts.ini all.yml \
-  --tags actions-runner \
-  --limit server-staging \
-  -e "runner_token=${runner_token}"
+# ansible-playbook -i inventory/hosts.ini all.yml --tags preconfig
+# ansible-playbook -i inventory/hosts.ini all.yml --tags users-init --limit server-staging
 
-read -p "Введите токен для server-production: " v_token
-ansible-playbook -i inventory/hosts.ini all.yml \
-  --tags actions-runner \
-  --limit server-production \
-  -e "runner_token=${runner_token}"
+# read -p "Введите токен для server-staging: " runner_token
+# ansible-playbook -i inventory/hosts.ini all.yml \
+#   --tags actions-runner \
+#   --limit server-staging \
+#   -e "runner_token=${runner_token}"
+
+# read -p "Введите токен для server-production: " v_token
+# ansible-playbook -i inventory/hosts.ini all.yml \
+#   --tags actions-runner \
+#   --limit server-production \
+#   -e "runner_token=${runner_token}"
--- vagrant-up.sh
+++ vagrant-up.sh
@@ -48,120 +48,65 @@ mkdir -p /home/root/vagrant
 cd /home/root/vagrant
 
 # Интерактивный ввод конфигураций сервера
-echo -e "\e[1m\e[35mВведите данные для конфигурации production сервера\e[0m"
-echo -ne "\e[3mИмя сервера: \e[0m"
-read hostname1
-echo -ne "\e[3mИмя сервера в VirtualBox: \e[0m"
-read vboxservername1
-echo -ne "\e[3mИмя администратора: \e[0m"
-read username1
-echo -ne "\e[3mПароль администратора: \e[0m"
-read -s password1
-clear
-echo -e "\e[1m\e[35mВведите данные для конфигурации staging сервера\e[0m"
-echo -ne "\e[3mИмя сервера: \e[0m"
-read hostname2
-echo -ne "\e[3mИмя сервера в VirtualBox: \e[0m"
-read vboxservername2
-echo -ne "\e[3mИмя администратора: \e[0m"
-read username2
-echo -ne "\e[3mПароль администратора: \e[0m"
-read -s password2
+echo -e "\e[1m\e[35mВведите данные для администратора\e[0m"
+echo -ne "\e[3mИмя: \e[0m"
+read username
+echo -ne "\e[3mПароль: \e[0m"
+read -s password
 clear
 
 # Создание Vagrantfile
 cat > Vagrantfile << EOF
 Vagrant.configure("2") do |config|
-    config.vm.box = "generic/ubuntu2204"
+    (1..3).each do |i|
+        config.vm.box = "generic/ubuntu2204"
+        
+        # Общие настройки SSH (для пользователя vagrant)
+        config.ssh.insert_key = true  # Генерировать новый ключ
+
+        config.vm.boot_timeout = 600
+
+        config.vm.define "vm#{i}" do |vm|
+            vm.vm.hostname = "server#{i}"
+            vm.ssh.host = "localhost"
+            vm.ssh.username = "vagrant"
+            vm.ssh.port = "222#{i}"
+
+            # Проброс порта: 2222 → 22 (для подключения по ssh)
+            vm.vm.network "forwarded_port", 
+                guest: 22,
+                host: 2220 + i,
+                id: "ssh",
+                auto_correct: false
+            
+            # Внутренняя сеть
+            vm.vm.network "private_network",
+                type: "dhcp",                  # Используем DHCP вместо статики
+                virtualbox__intnet: "intnet#{i}00"
+            
+            # Настройки VirtualBox
+            vm.vm.provider "virtualbox" do |vb|
+                vb.name = "ubuntu-server-#{i}"
+                vb.memory = 2048
+                vb.cpus = 1
+        end
     
-    # Общие настройки SSH (для пользователя vagrant)
-    config.ssh.insert_key = true  # Генерировать новый ключ
+        # Создание пользователя admin с паролем
+        vm.vm.provision "shell", inline: <<-SHELL
+            # Обновление sshd
+            sudo apt update
+            sudo apt-get install --only-upgrade openssh-server -y
+            sudo systemctl enable sshd
 
-    config.vm.boot_timeout = 600
-
-    # Первая VM - server1
-    config.vm.define "vm1" do |vm1|
-      vm1.vm.hostname = "$hostname1"
-      vm1.ssh.host = "localhost"
-      vm1.ssh.username = "vagrant"
-      vm1.ssh.port = "2222"
-
-      # Проброс порта: 2222 → 22 (для подключения по ssh)
-      vm1.vm.network "forwarded_port", 
-        guest: 22,
-        host: 2222,
-        id: "ssh",
-        auto_correct: false
-      
-      # Внутренняя сеть
-      vm1.vm.network "private_network",
-          type: "dhcp",                  # Используем DHCP вместо статики
-          virtualbox__intnet: "intnet100"
-      
-      # Настройки VirtualBox
-      vm1.vm.provider "virtualbox" do |vb|
-        vb.name = "$vboxservername1"
-        vb.memory = 2048
-        vb.cpus = 1
-      end
-  
-      # Создание пользователя admin с паролем
-      vm1.vm.provision "shell", inline: <<-SHELL
-        # Обновление sshd
-	      sudo apt update
-	      sudo apt-get install --only-upgrade openssh-server -y
-	      sudo systemctl enable sshd
-        # Создание нового пользователя admin
-	      sudo useradd -m -s /bin/bash $username1
-        echo "$username1:$password1" | sudo chpasswd
-        sudo usermod -aG sudo $username1
-  
-        # Разрешаем парольный SSH только для admin
-        sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
-        sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
-        sudo passwd -d vagrant
-  
-        # Удаляем пароль для пользователя vagrant (оставляем только ключ)
-        sudo passwd -d vagrant
-      SHELL
-    end
-  
-    # Вторая VM - server2 (аналогично)
-    config.vm.define "vm2" do |vm2|
-      vm2.vm.hostname = "$hostname2"
-      vm2.ssh.host = "localhost"
-      vm2.ssh.username = "vagrant"
-      vm2.ssh.port = "2223"
-
-      # Проброс порта: 2222 → 22 (для ручного подключения)
-      vm2.vm.network "forwarded_port", 
-        guest: 22,
-        host: 2223,
-        id: "ssh",
-        auto_correct: false
-    
-        # Внутренняя сеть
-      vm2.vm.network "private_network",
-          type: "dhcp",                  # Используем DHCP вместо статики
-          virtualbox__intnet: "intnet200"
-      
-      vm2.vm.provider "virtualbox" do |vb|
-        vb.name = "$vboxservername2"
-        vb.memory = 2048
-        vb.cpus = 1
-      end
-  
-      vm2.vm.provision "shell", inline: <<-SHELL
-	      sudo apt update
-	      sudo apt-get install --only-upgrade openssh-server -y
-	      sudo systemctl enable sshd
-	      sudo useradd -m -s /bin/bash $username2
-        echo "$username2:$password2" | sudo chpasswd
-        sudo usermod -aG sudo $username2
-        sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
-        sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
-        sudo passwd -d vagrant
-      SHELL
+            # Создание нового пользователя admin
+            sudo useradd -m -s /bin/bash $username
+            echo "$username:$password" | sudo chpasswd
+            sudo usermod -aG sudo $username
+     
+            # Удаляем пароль для пользователя vagrant (оставляем только ключ)
+            sudo passwd -d vagrant
+        SHELL
+        end
     end
 end
 EOF
@@ -171,27 +116,34 @@ vagrant up && echo -e "\e[32m\e[1mВиртуальные машины успеш
 || echo -e "\e[31m\e[1mОшибка при создании виртуальных машин!\e[0m"
 
 # Выключение VM
-sudo vboxmanage controlvm "$vboxservername1" acpipowerbutton
-sudo vboxmanage controlvm "$vboxservername2" acpipowerbutton
+sudo vboxmanage controlvm "ubuntu-server-1" acpipowerbutton
+sudo vboxmanage controlvm "ubuntu-server-2" acpipowerbutton
+sudo vboxmanage controlvm "ubuntu-server-3" acpipowerbutton
 
 # Ждем пока VM выключится
-while [ "$(sudo vboxmanage showvminfo "$vboxservername1" --machinereadable | grep 'VMState=')" != 'VMState="poweroff"' ]; do
+while [ "$(sudo vboxmanage showvminfo "ubuntu-server-1" --machinereadable | grep 'VMState=')" != 'VMState="poweroff"' ]; do
     sleep 5
 done
 
-while [ "$(sudo vboxmanage showvminfo "$vboxservername2" --machinereadable | grep 'VMState=')" != 'VMState="poweroff"' ]; do
+while [ "$(sudo vboxmanage showvminfo "ubuntu-server-2" --machinereadable | grep 'VMState=')" != 'VMState="poweroff"' ]; do
+    sleep 5
+done
+
+while [ "$(sudo vboxmanage showvminfo "ubuntu-server-3" --machinereadable | grep 'VMState=')" != 'VMState="poweroff"' ]; do
     sleep 5
 done
 
 # Отключение ненужных адаптеров у VM 
-sudo vboxmanage modifyvm "$vboxservername1" --nic1 none
-sudo vboxmanage modifyvm "$vboxservername2" --nic1 none
+sudo vboxmanage modifyvm "ubuntu-server-1" --nic1 none
+sudo vboxmanage modifyvm "ubuntu-server-2" --nic1 none
+sudo vboxmanage modifyvm "ubuntu-server-3" --nic1 none
 
 sleep 2
 
 # Запуск VM
-sudo vboxmanage startvm "$vboxservername1" --type headless
-sudo vboxmanage startvm "$vboxservername2" --type headless
+sudo vboxmanage startvm "ubuntu-server-1" --type headless
+sudo vboxmanage startvm "ubuntu-server-2" --type headless
+sudo vboxmanage startvm "ubuntu-server-3" --type headless
 
 sleep 5
 
